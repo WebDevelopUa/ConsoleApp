d233e0d0f81541b91519809c58c7ccbb40d50e6c
WebDev <js.develop.com@gmail.com>
2018-05-07 22:53:44 +0300

    Change console text

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,8 @@ namespace ConsoleApp1
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hi There ;)\nGood Day");
+            Console.ReadKey();
         }
     }
 }
