6b1d35998610d6183afac486f33b738d4db15094
WebDev <js.develop.com@gmail.com>
2018-05-07 23:51:59 +0300

    Add Sum Calculator

---
+++ ConsoleCalculator/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleSumCalculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num, num_1;
+
+            Console.WriteLine("Enter first number:");
+            num = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("\nEnter second number:");
+            num_1 = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.WriteLine("\nThe sum of two numbers is: " + (num + num_1).ToString());
+            Console.ReadKey();
+
+
+
+        }
+    }
+}
---
+++ ProjectOne/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace ProjectOne
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            int num = -10;
+            uint num_1 = 10;
+            float f = 1.23f;
+            bool boolean = true;
+            string name = "Jora";
+            char n = 'J';
+
+            Console.WriteLine(num);
+            Console.WriteLine(num_1);
+            Console.WriteLine(f);
+            Console.WriteLine(boolean);
+            Console.WriteLine(name);
+            Console.WriteLine(n);
+            Console.ReadLine();
+
+
+
+        }
+    }
+}
