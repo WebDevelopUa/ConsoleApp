3e166faea0aec8de4b78fd38aa3c1d249095f590
WebDev <js.develop.com@gmail.com>
2018-05-08 02:13:53 +0300

    Add Check Lucky number project

---
+++ CheckNumber/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace CheckNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double num;
+ 
+
+            Console.WriteLine("Enter number:");
+            num = Convert.ToDouble(Console.ReadLine());
+
+            if (num > 18 && num < 50)
+                Console.WriteLine("Access GRANTED, you chose range between 18 and 50");
+
+            else if (num < 18)
+                Console.WriteLine("Access DENIED, you chose range below 18");
+
+            else if (num > 50)
+                Console.WriteLine("Access DENIED, you chose range above 50");
+
+            //else if (num == 18)
+            //    Console.WriteLine("Access GRANTED, you chose 18");
+
+            //else if (num == 50)
+            //    Console.WriteLine("Access GRANTED, you chose 50");
+
+            else
+                Console.WriteLine("Congrats! You WIN! " + num + " is a lucky number for today");
+ 
+
+            Console.ReadKey();
+        }
+    }
+}
