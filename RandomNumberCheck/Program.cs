2dd9fc7ae41ea8535a3898c952766f3426ec587e
WebDev <js.develop.com@gmail.com>
2018-05-08 03:16:11 +0300

    Add Random lottery number check

---
+++ RandomNumberCheck/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace RandomNumberCheck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rndNum = new Random();
+            int num;
+            num = rndNum.Next(1, 10);
+
+            Console.WriteLine("Today's Winner is Random selected number: " + num);
+
+            switch (num)
+            {
+                case 1:
+                    Console.WriteLine("Jonny is a Winner!");
+                    break;
+                case 2:
+                    Console.WriteLine("Nancy is a Winner!");
+                    break;
+                case 3:
+                    Console.WriteLine("Lucy is a Winner!");
+                    break;
+                default:
+                    Console.WriteLine("Nobody wins, try again next time");
+                    break;
+
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
